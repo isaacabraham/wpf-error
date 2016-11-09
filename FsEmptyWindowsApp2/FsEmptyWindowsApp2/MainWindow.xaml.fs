8fe91a48d7c3f1fc3d25b06b102b9147f9ff8343
Isaac Abraham <isaac.abraham@gmail.com>
2016-11-09 20:55:42 +0100

    Commit.

---
+++ FsEmptyWindowsApp2/FsEmptyWindowsApp2/MainWindow.xaml.fs
@@ -0,0 +1,15 @@
+﻿namespace ViewModels
+
+open System
+open System.Windows
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
+open FsXaml
+
+type MyConverter() =
+    inherit ConverterBase(fun _ _ _ _ -> 10 :> obj)
+
+type MainView = XAML<"MainWindow.xaml">
+
+type MainViewModel() = 
+    inherit ViewModelBase()
